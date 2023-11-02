aa3b65d7fce8fa374dedbcbc77e45f67707a3c86
luthfi <alfarisi.luthfi@yahoo.com>
2023-11-02 16:55:46 +0700

    first commit

---
+++ src/main/java/com/servlet/HelloServlet.java
@@ -0,0 +1,27 @@
+package com.servlet;
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * HelloServlet
+ */
+@WebServlet(urlPatterns = {"/hello"})
+public class HelloServlet extends HttpServlet{
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        // RequestDispatcher dispacther = req.getRequestDispatcher("templates/hello.jsp");
+        // dispacther.forward(req, resp);
+        resp.getWriter().println("hello");
+
+    }
+    
+}
