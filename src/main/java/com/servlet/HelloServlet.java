27348d4afc0d689c7615f8c13cd6df01468f7dc2
luthfi <alfarisi.luthfi@yahoo.com>
2023-11-04 21:33:51 +0700

    test servlet

--- src/main/java/com/servlet/HelloServlet.java
+++ src/main/java/com/servlet/HelloServlet.java
@@ -11,14 +11,14 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * HelloServlet
  */
-@WebServlet(urlPatterns = {"/hello"})
+@WebServlet(urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet{
 
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // TODO Auto-generated method stub
-        // RequestDispatcher dispacther = req.getRequestDispatcher("templates/hello.jsp");
+        // RequestDispatcher dispacther = req.getRequestDispatcher("hello.jsp");
         // dispacther.forward(req, resp);
         resp.getWriter().println("hello");
 
